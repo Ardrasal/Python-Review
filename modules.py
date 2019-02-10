2478d2f97ea9f0bf15f9d7dae638dda607210ce2
Ardra Salemy <ardrasalemy@gmail.com>
2019-02-10 15:40:47 -0500

    Adds CamelCase module and quick modules review.

--- modules.py
+++ modules.py
@@ -1,7 +1,23 @@
 # A module is basically a file containing a set of functions to include in your application. There are core python modules, modules you can install using the pip package manager (including Django) as well as custom modules
 
-import datetime
+# import datetime
+# or
+from datetime import date
+import time
+# or
+from time import time
 
-today = datetime.date.today()
+# Pip module
+from camelcase import CamelCase  # pipenv install camelcase
 
-print(today)
+c = CamelCase()
+print (c.hump('hello there world'))
+
+today = date.today()
+# today = datetime.date.today()
+# timestamp = time.time()
+# using line 8
+timestamp = time()
+
+# print(today)
+print(timestamp)
